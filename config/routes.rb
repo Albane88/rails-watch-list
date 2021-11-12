44485a31674381d57ae4288053d5eed960a0e2c1
albane orts <albaneorts@gmail.com>
2021-11-12 12:33:42 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'lists/new', to: 'lists#new'
+  # post 'lists', to: 'lists#create'
+  # get 'lists', to: 'lists#index'
+  # get 'lists/:id', to: 'lists#show', as: :list
+  root to: "lists#index"
+  resources :lists, only: [:new, :create, :show] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
