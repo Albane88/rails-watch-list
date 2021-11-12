7e13cb2dfed7ebe6244fbbfaf941360b6ed69dd6
albane orts <albaneorts@gmail.com>
2021-11-12 15:51:23 +0100

    yey

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,6 @@
 List.destroy_all
 Movie.destroy_all
 
-10.times do
   url = 'http://tmdb.lewagon.com/movie/top_rated'
   movies = open(url).read
   movie = JSON.parse(movies)
@@ -19,5 +18,4 @@ Movie.destroy_all
   new_movie = Movie.create(title: movie['title'], overview: movie['overview'], poster_url: movie_poster + movie['poster_path'], rating: movie['vote_average'])
   puts "#{new_movie.title} has been created!"
   new_movie.save!
-  end
 end
